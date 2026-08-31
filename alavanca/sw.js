6c264f15592c7741f4e0ddeaa27057dff2219eac
Juliano Rodrigues <julianordg.x@gmail.com>
2026-08-31 16:07:33 -0300

    Cache all Alavanca app assets for offline use

--- alavanca/sw.js
+++ alavanca/sw.js
@@ -1,5 +1,5 @@
-const CACHE='alavanca-v32-pages-1';
-const ASSETS=['./','./index.html','./style.css','./core.js','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='alavanca-v32-pages-2';
+const ASSETS=['./','./index.html','./style.css','./core.js','./app.js','./app.part1','./app.part2','./app.part3','./app.part4','./app.part5','./app.part6','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(res=>{if(res&&res.ok){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return res;}).catch(()=>e.request.mode==='navigate'?caches.match('./index.html'):Response.error())));});
